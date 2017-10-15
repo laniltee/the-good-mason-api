3b094207ed8caeb27eeb9d33cde63166d069016c
Lanil Marasinghe <Lanil Marasinghe>
2017-10-15 19:59:42 +0530

    first commit

---
+++ src/main/java/lk/sliit/TheGoodMasonApiApplication.java
@@ -0,0 +1,12 @@
+package lk.sliit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGoodMasonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGoodMasonApiApplication.class, args);
+	}
+}
